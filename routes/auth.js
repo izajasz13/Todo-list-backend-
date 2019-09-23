315b56eabd4b231b0ace8f2e82a237e5734fd308
izajasz13 <karoldd13@gmail.com>
2019-09-23 12:15:28 +0200

    Fixed task list

--- routes/auth.js
+++ routes/auth.js
@@ -23,7 +23,8 @@ router.post('/register', async (req, res, next) => {
     const user = new User({
         name: req.body.name,
         email: req.body.email,
-        password: hashedPassword
+        password: hashedPassword,
+        taskList: []
     });
     try{
         const saveUser = await user.save();
